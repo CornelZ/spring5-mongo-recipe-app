d255b2bf1d1c7703a72a350390abef8876a8a7e2
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-25 14:48:17 +0300

    cleanup

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,38 +1,34 @@
 package guru.springframework.domain;
 
+import java.math.BigDecimal;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 
-import java.math.BigDecimal;
-
-/**
- * Created by jt on 6/13/17.
- */
 @Getter
 @Setter
 public class Ingredient {
 
-    private String id;
-    private String description;
-    private BigDecimal amount;
+  private String id = UUID.randomUUID().toString();
 
-    private UnitOfMeasure uom;
-    private Recipe recipe;
+  private String description;
+  private BigDecimal amount;
+  @DBRef private UnitOfMeasure uom;
+  private Recipe recipe;
 
-    public Ingredient() {
-    }
+  public Ingredient() {}
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-    }
+  public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+    this.description = description;
+    this.amount = amount;
+    this.uom = uom;
+  }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-        this.recipe = recipe;
-    }
-
+  public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    this.description = description;
+    this.amount = amount;
+    this.uom = uom;
+    this.recipe = recipe;
+  }
 }
--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -2,15 +2,14 @@ package guru.springframework.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-
-/**
- * Created by jt on 6/13/17.
- */
 @Getter
 @Setter
+@Document
 public class UnitOfMeasure {
 
-    private String id;
-    private String description;
+  @Id private String id;
+  private String description;
 }
